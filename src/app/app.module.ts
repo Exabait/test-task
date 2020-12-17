7ebd4dd9645673929afbd571b19479a4e638dc35
Exabait <v.step2327@gmail.com>
2020-12-17 17:48:35 +0200

    clear module and fix mime/type issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { ImagePlaceholderComponent } from './components/image-placeholder/image-placeholder.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
-
 import { InputComponent } from './components/input/input.component';
 import { CardComponent } from './components/card/card.component';
 import { ContentDialogComponent } from './components/content-dialog/content-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { LocaleService, NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+
 
 @NgModule({
   declarations: [
@@ -33,10 +32,9 @@ import { LocaleService, NgxDaterangepickerMd } from 'ngx-daterangepicker-materia
     FormsModule,
     BrowserAnimationsModule,
     MatDialogModule,
-    NgxDaterangepickerMd.forRoot(),
     ReactiveFormsModule
   ],
-  providers: [LocaleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
